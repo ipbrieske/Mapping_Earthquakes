d373eda6d05eb31cbbb156245c7c06ca5431862e
Isaac Brieske <ipbrieske@gmail.com>
2022-05-22 23:55:52 -0700

    added color conditionals to earthquake markers

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -38,8 +38,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
             console.log(data);
             return L.circleMarker(latlng);
         },
-        style: styleInfo
-        
+        style: styleInfo,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+          }
     }).addTo(map);
 });
 
@@ -50,7 +52,7 @@ function styleInfo(feature) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
@@ -67,7 +69,25 @@ function getRadius(magnitude) {
     return magnitude * 4;
 };
 
-
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }
 
 // Then we add our controller with map layers
 L.control.layers(baseMaps).addTo(map);
